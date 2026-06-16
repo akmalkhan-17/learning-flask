bc588f1825713e2119d0bd736544d8a0d581b30a
akmalkhan-17 <akmalkhancontact@gmail.com>
2026-06-17 00:41:17 +0530

    some shit

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,13 @@
 # ill be handling auth related routes here
 from flask import Blueprint
+from flask import render_template
 
 auth_bp = Blueprint("auth", __name__)
 
 
 @auth_bp.route("/login")
 def login():
-    return "Login Page"
+    return render_template("login.html")
 
 
 @auth_bp.route("/register")
